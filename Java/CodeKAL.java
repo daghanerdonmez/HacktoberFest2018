af3032b08775b9dc009522d4da148f17574c56a5
daghanerdonmez <44506702+daghanerdonmez@users.noreply.github.com>
2018-10-26 22:08:04 +0300

    CodeKAL.java
    Çöp olan bazı şeylerin çöp olduğunu çöpçülerin yüzüne vurdum, vurmak zorunda kaldım.

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -1,12 +1,47 @@
 public class CodeKal {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Java çöp bu yüzden aşağıya güzel bir python porogramı salıyorum thank me later.");
+		
+		/*	def faktoriyel(sayi):
 
-		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretlerinin Java için 
-		// özel anlamları var. Çevirirken silmeyiniz.)
+			    faktoriyel = 1
 
-		System.out.println("CodeKAL'ı çok seviyorum.");
-		System.out.println("CodeKAL'da olduğum için çok mutluyum.");
+			    for i in range(1, sayi + 1):
+				faktoriyel *= i
+
+			    return faktoriyel
+
+
+			islem = input("Hangi formülü kullanmak istiyorsunuz? FAKTORIYEL [F], PERMUTASYON [P], KOMBINASYON [K]").lower()
+
+			if islem == "f":
+			    fak_sayi1 = int(input("Hangi sayının faktoriyelini almak istersiniz?"))
+			    sonuc_fak = faktoriyel(fak_sayi1)
+			    print(sonuc_fak)
+
+			elif islem == "p":
+			    per_sayi1 = int(input("Permütasyonun ilk sayısını girin:"))
+			    per_sayi2 = int(input("Permütasyonun ikinci sayısını girin:"))
+			    sonuc_per1 = faktoriyel(per_sayi1)
+			    sonuc_per2 = faktoriyel(per_sayi1 - per_sayi2)
+			    final_per = sonuc_per1/sonuc_per2
+			    print(final_per)
+
+
+			elif islem == "k":
+			    kom_sayi1 = int(input("Kombinasyonun ilk sayısını girin:"))
+			    kom_sayi2 = int(input("Kombinasyonun ikinci sayısını girin:"))
+			    sonuc_kom1 = faktoriyel(kom_sayi1)
+			    sonuc_kom2 = faktoriyel(kom_sayi2)
+			    sonuc_kom3 = faktoriyel(kom_sayi1 - kom_sayi2)
+			    sonuc_kom4 = sonuc_kom3 * sonuc_kom2
+			    final_kom = sonuc_kom1/sonuc_kom4
+			    print(kom_sayi1, "'in ", kom_sayi2, "'li kombinasyonu ", final_kom, "'dur.")
+
+			else:
+			     print("Lütfen geçerli bir harf girin") */
 
 	}
 
